e118ba707b9f31e9f957078dba2ca27ce3c42c1e
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-04-13 02:12:16 +0530

    frontend updated for chatApp room functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,119 +1,62 @@
-import {useState, useRef, useEffect} from "react"
-import { IoPersonCircleOutline } from "react-icons/io5";
-import { IoSend } from "react-icons/io5";
-
+import { useState, useEffect, useRef} from "react"
+// import ChatBoxComponent from "./components/chatBox"; 
+import { BsChat } from "react-icons/bs";
+import ChatBoxComponent from "./components/chatBox";
+import {nanoid} from "nanoid"
 
 function App() {
 
-  // So I want whenever my chat application mounts I want to establish the websockets initialising TCP connection.
-  interface CHAT {
-    user?:string,
-    reply?:string
+  const [room, setRoom] = useState(false);
+  const [roomId, setRoomId] = useState("");
+  const inpRef = useRef<HTMLInputElement>(null);
+  const user = "aauishi";
+  const userId = "XYZ123";
+  const userAvatar = "haha";
+
+  function joinRoom(){
+    let id = inpRef.current?.value as string;   
+    setRoom(true);
+    setRoomId(id);
   }
 
-  const [socket, setSocket] = useState<WebSocket>();
-  // const [chat, setChat] = useState([
-  //   {user:"dummy Chat",reply:"dummy Reply"}
-  // ]);
-  const [chat, setChat] = useState<CHAT[]>([]);
-  const sendRef = useRef<HTMLDivElement>(null);
-  const inpRef = useRef<HTMLInputElement>(null);
-  const replyRef = useRef<HTMLDivElement>(null);
-  
+  function createRoom(){
+    const id = nanoid(5);
+    setRoom(true);
+    setRoomId(id);
 
-
-  
-    function callWebSocket():void{
-      // setChat(t => t.push(inpRef.current.value))
-
-      if(!inpRef.current?.value){
-        return;
-      }
-      socket?.send(inpRef.current?.value);
-      
-      let m = [...chat,
-        {user: inpRef.current?.value} ];
-      setChat(m);
-
-      inpRef.current.value=""
-      
-    }
-
-  useEffect(function(){
-    const ws = new WebSocket("ws://localhost:8000");
-    setSocket(ws);
-    /* ws.onerror = () => {
-
-    }
-
-    ws.close = () => {
-
-    }
-
-    ws.onopen = () => {
-
-    } */
-
-    ws.onmessage = (ev) => {
-      if(ev.data === "WebSocket connection established"){
-        alert(ev.data);
-      }
-      // else if(ev.data === "pong"){
-      //   setChat(prevChat => [...prevChat, {reply:ev.data}]);
-      //   console.log(chat)
-      // }
-      else{
-        setChat(prevChat => [...prevChat, {reply:ev.data}]);
-        console.log(chat)
-      }
-    }
-    // console.log(chat);
-
-  },[])
-
-  
-
+  }
 
   return (
-    <div className="flex justify-center items-center w-full min-h-screen h-full border border-solid border-black  p-10 flex-col gap-2">
-      <h1 className="text-4xl h-full font-bold text-gray-700">| BROADCASTING CHAT APP |</h1>
-      <div className="flex flex-col justify-end items-end border border-gray-900 border-solid p-4 min-h-[80%] h-full w-[80%] gap-4 bg-blue-100 rounded-lg">
-        <div className=" flex gap-2 flex-col w-full h-full min-h-[90%] p-4">
-          {
-            chat.map((e,index)=>{
-              // console.log(e, chat[index])
-              return <div key={index} className="flex flex-col gap-2">
-               
-               {e.user && <div ref={sendRef} className="flex justify-end items-start text-gray-700 gap-2" > 
-                <div className="w-fit max-w-[80%] px-2 py-1 rounded-2xl bg-blue-500 text-white">
-                {e.user}
-                </div>
-                <IoPersonCircleOutline className="text-2xl" />
-                </div>
-               }
-                
-                  
+    <div className="flex justify-start bg-slate-200 items-center flex-col gap-8 min-h-screen h-full p-5 border-4 border-solid border-black">
 
-                {e.reply &&  <div ref={replyRef} className="flex items-start gap-2">
-                  <IoPersonCircleOutline className="text-2xl" />
-                  <div className="w-fit max-w-[80%] px-2 py-1 rounded-2xl bg-blue-500 text-white">
-                  {
-                    e.reply
-                  }
-                  </div>
-                </div>}
-                
-              </div>
-            })
-          }
+      {
+        !room && <>
+        <div className="flex flex-col">
+          <div className="font-bold text-6xl">ChatRoom</div>
+          <div className="font-semibold text-xs">Welcome to Real Time Communication with GROUPS!!</div>
         </div>
-        <div className="flex w-full gap-2 justify-center">
-          <input ref={inpRef} type="text" placeholder="type message.." className="w-full bg-gray-600 py-2 px-4 h-fit outline-none text-white ">
-          </input>
-          <button className="bg-blue-500 py-1 px-4 rounded-lg active:scale-90 text-white" onClick={callWebSocket}><IoSend /></button>
+        <div className="bg-blue-200 w-[50%] h-fit flex gap-2 flex-col p-10 rounded-lg shadow-lg">
+          <div className="flex flex-col">
+            <div className="flex items-center font-extrabold text-2xl leading-[0.5] gap-2">
+              <BsChat />
+              <p> Real Time Chat</p>
+            </div>
+            <p className="text-xs font-medium">temporary room that expires after both users exit</p>
+          </div>
+          <button className=" bg-black rounded-lg  text-white active:scale-95 py-1 " onClick={createRoom}>Create New Room</button>
+          <div className="flex w-full justify-center gap-2">
+            <input ref={inpRef} type="text" className="outline-none w-full px-2 py-1 rounded-lg bg-blue-100" placeholder="Enter Room Code"></input>
+            <button className="bg-black text-white px-2 py-1 rounded-lg w-[20%] active:scale-95" onClick={joinRoom}>Join Room</button>
+          </div>
+  
         </div>
-      </div>
+        </>
+      }
+
+      {room && <ChatBoxComponent setRoom={setRoom} roomId={roomId} user={user} userId={userId} userAvatar={userAvatar} ></ChatBoxComponent>}
+      
     </div>
+
   )
 }
 
